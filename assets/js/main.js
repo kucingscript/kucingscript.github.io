acc73b6ce3244ded680471ac6480acbca87e37d3
Ar Rasyid Sarifullah <rasyidzkun@gmail.com>
2020-11-03 11:37:06 +0700

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+//scroll
+
+$('.page-scroll').on('click', function(e) {
+
+    var tujuan = $(this).attr('href');
+    var elemenTujuan = $(tujuan);
+
+    $('html, body').animate({
+        scrollTop:elemenTujuan.offset().top - 60
+    }, 1250, 'easeInOutExpo');
+
+    e.preventDefault();
+});
+
+
+//aos
+
+function aos_init() {
+    AOS.init({
+        duration: 1000,
+        once: true
+    });
+}
+$(window).on('load', function() {
+    aos_init();
+});
+
+//tooltip
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+//preloader
+
+$(window).on('load', function() {
+    if ($('.preloader').length) {
+        $('.preloader').delay(100).fadeOut('slow', function() {
+        $(this).remove();
+        });
+    }
+});
+
+
+  // Back to top button
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+
+$('.back-to-top').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+});
